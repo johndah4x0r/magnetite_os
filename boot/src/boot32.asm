90d9b2afdfbf7a3d80a2a5b1de4835903362c5af
John Isaac Calderon <ogclapdrop@gmail.com>
2025-08-08 19:26:16 +0200

    Added PM-to-LM routine + setup for split build
    The PM-to-LM routine hasn't been tested, but it doesn compile,
    and object files generated by `nasm` are linkable to some
    extent.
    
    Link scripts will be added in the near future. The Makefile
    will be populated to simplify future testing.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/boot32.asm
+++ boot/src/boot32.asm
@@ -3,8 +3,58 @@
 ; Do not use absolute positioning, as the
 ; binaries are linked at a later stage
 
+extern _binary_boot64_bin_start     ; Wrapped 64-bit code label
+
+; Flags for later use
+ID_EFLAGS       equ 0x00200000      ; EFLAGS ID bit
+EXT_CPUID       equ 0x80000000      ; CPUID extensions
+FEAT_CPUID      equ 0x80000001      ; CPUID extended features
+LM_EDX_CPUID    equ 0x20000000      ; Long mode bit
+NO_PAGING       equ 0x7fffffff      ; No 32-bit paging
+
+PAE_ENABLE      equ 1 << 5          ; Enable PAE in CR4
+PG_ENABLE       equ 1 << 31         ; Enable paging in CR0
+
+
+; Page hierarchy layout
+PML4T_ADDR      equ 0x1000          ; Location of PML4 table (master hierarchy)
+PDPT_ADDR       equ 0x2000          ; Location of PDP table (huge)
+PDT_ADDR        equ 0x3000          ; Location of page directory table (large)
+PT_ADDR         equ 0x4000          ; Location of page table (standard)
+
+
+; Page masks and flags
+PT_ADDR_MASK    equ 0xffffffffff000 ; Mask to align addresses to 4 kiB
+PT_PRESENT      equ 1               ; Marks page as present
+PT_READWRITE    equ 2               ; Marks page as R/W
+PT_PAGESIZE     equ 128             ; Marks page as large/huge (if needed)
+
+SIZEOF_PAGE     equ 0x2000000       ; Sets page size to 2 MiB
+SIZEOF_PT       equ 0x1000          ; Sets page table size to 4 kiB
+
+ENTRIES_PER_PT  equ 512             ; Entries per page table
+SIZEOF_PT_ENTRY equ 8               ; Size of PT entry (64 bits)
+
+EFER_MSR        equ 0xC0000080      ; EFER MSR address
+EFER_LME        equ 0x100           ; EFER IA-32e set bit
+
+
+; GDT bits
+
+; - access bits
+SEG_PRESENT        equ 1 << 7
+SEG_NOT_SYS        equ 1 << 4
+SEG_EXEC           equ 1 << 3
+SEG_RW             equ 1 << 1
+
+; - flags bits
+SEG_GRAN_4K       equ 1 << 7
+SEG_SZ_32         equ 1 << 6
+SEG_LONG_MODE     equ 1 << 5
+
+
 ; Preamble section
-section .text.preamble
+section .preamble
 _preamble:
     ; Reuse legacy magic sequence
     jmp dword _boot32
@@ -14,14 +64,14 @@ _preamble:
     times 16-($-$$) nop
 
 ; ---- context ---- ;
-; - we intend to place .text.hal
-;   in between .text.preamble and
-;   .text.boot
+; - we intend to place .hal_vt
+;   in between .preamble and
+;   .boot32
 ; - the vector list can be defined
 ;   in .data, so long as the
 ;   kernel can locate it later
 
-section .text.boot32
+section .boot32
 _boot32:
     ; Kill interrupts (if they are still active)
     cli
@@ -51,7 +101,7 @@ check_cpuid:
     mov ecx, eax                ; Store copy of EFLAGS
 
     ; Attempt to flip the ID bit in EFLAGS
-    xor eax, 0x200000           ; Flip bit 21
+    xor eax, ID_EFLAGS          ; Flip bit 21
     push eax                    ; Store modified EFLAGS
     popfd                       ; Copy from stack
 
@@ -72,6 +122,103 @@ check_cpuid:
     jmp panicb                  ; Panic - never to return...
 .end:
 
+; Check if long mode is supported
+check_lm:
+    ; Check if CPUID supports extended features
+    mov eax, EXT_CPUID          ; Check highest EAX parameter
+    cpuid
+    cmp eax, FEAT_CPUID         ; If EAX < FEAT_CPUID, no dice...
+    jb .no_lm
+
+    ; Check if long mode is supported
+    mov eax, FEAT_CPUID         ; Check for features
+    cpuid
+    test edx, LM_EDX_CPUID      ; Check if the LM bit is set
+    jnz .end                    ; Exit if it is set
+    ; --- fall-through --- ;
+.no_lm:
+    ; Panic if long mode is not supported
+    lea esi, [msgs.no_lm]       ; Load pointer to reason
+    jmp panicb                  ; Panic - never to return...
+.end:
+
+; Disable 32-bit paging
+; (unlikely that paging is up, since
+; *we* control the MBR source)
+disable_paging32:
+    mov eax, cr0                ; Load CR0 into EAX
+    and eax, NO_PAGING          ; Unset paging bit
+    mov cr0, eax                ; Store modified CR0 
+
+; Initialize 64-bit paging
+; - essentially the same thought process
+;   as the one outlined in 'wiki.osdev.org'
+; TODO: maybe consider higher-half mapping
+;       in addition to identity mapping
+init_paging:
+    ; Clear the master page hierarchy
+    lea edi, [PML4T_ADDR]       ; Point EDI to the highest table
+    mov cr3, edi                ; Let the CPU know where the tables are
+
+    ; Write 4 * 1kiB, which should cover
+    ; all four hierarchy levels
+    xor eax, eax
+    mov ecx, SIZEOF_PT
+    rep stosd
+
+    mov edi, cr3                ; Reset EDI back to the highest table
+
+.set_flags:
+    ; Set flags for each level
+    ; - EDI is equal to PML4 address
+    mov dword [edi], PDPT_ADDR & PT_ADDR_MASK | PT_PRESENT | PT_READWRITE
+
+    mov edi, PDPT_ADDR
+    mov dword [edi], PDT_ADDR & PT_ADDR_MASK | PT_PRESENT | PT_READWRITE
+
+    mov edi, PDT_ADDR
+    mov dword [edi], PT_ADDR & PT_ADDR_MASK | PT_PRESENT | PT_READWRITE
+
+.fill_pt:
+    ; Populate PT to identity-map 0-2 MiB
+    lea edi, [PT_ADDR]          ; Point EDI to PT 0
+
+    ; - set flags
+    ; - map PT 0, page 0 to 0-4 kiB
+    mov ebx, PT_PRESENT | PT_READWRITE
+    mov ecx, ENTRIES_PER_PT     ; Fill PT 0
+
+.set_entry_ident:
+    mov dword [edi], ebx        ; Write entry to [EDI]
+    add ebx, SIZEOF_PAGE        ; Map next physical page
+    add edi, SIZEOF_PT_ENTRY    ; Write to next entry
+    loop .set_entry_ident
+
+.enable_pae:
+    mov eax, cr4                ; Load CR4 into EAX
+    or eax, PAE_ENABLE          ; Enable PAE in CR4
+    mov cr4, eax                ; Store modified CR4
+
+enable_lm:
+    ; Enable IA-32e mode first
+    mov ecx, EFER_MSR
+    rdmsr
+    or eax, EFER_LME
+    wrmsr
+
+    ; Then enable paging
+    mov eax, cr0
+    or eax, PG_ENABLE
+    mov cr0, eax
+
+    ; Load 64-bit GDT and perform far jump
+    lgdt [gdt64.pointer]
+    jmp gdt64.code:_binary_boot64_bin_start
+
+    ; Halt in the event of a failure
+    ; (unreachable)
+    cli
+    hlt
 
 ; ---- Routines ---- ;
 
@@ -126,10 +273,33 @@ panicb:
 ; them anyways during linking)
 section .data.preboot
 
+; 64-bit GDT
+gdt64:
+    .null: equ $ - gdt64
+        dq 0
+    .code: equ $ - gdt64
+        .code.limit_lo   dw 0xffff
+        .code.base_lo    dw 0x0000
+        .code.base_mid   db 0x00
+        .code.access     db SEG_PRESENT | SEG_NOT_SYS | SEG_EXEC | SEG_RW
+        .code.sflags     db SEG_GRAN_4K | SEG_LONG_MODE | 0x0f
+        .code.base_high  db 0x00
+    .data: equ $ - gdt64
+        .data.limit_lo   dw 0xffff
+        .data.base_lo    dw 0x0000
+        .data.base_mid   db 0x00
+        .data.access     db SEG_PRESENT | SEG_NOT_SYS | SEG_RW
+        .data.sflags     db SEG_GRAN_4K | SEG_SZ_32 | 0x0f
+        .data.base_high  db 0x00
+    .pointer:
+        dw $ - gdt64 - 1
+        dd gdt64, 0
+
 ; Null-terminated messages
 msgs:
     .panic      db "Loader panicked while in 32-bit PM. Reason: ", 0
-    .no_cpuid   db "CPU does not appear to expose CPUID", 0
+    .no_cpuid   db "CPU appears to not support CPUID", 0
+    .no_lm      db "CPU appears to not support x86-64 long mode", 0
 
 ; Pointer to BPB (32-bit pointer extended to 64-bit)
 oem_label:
