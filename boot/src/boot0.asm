09dd032baa545b11fde96c189d7994a2defddf53
John Isaac Calderon <ogclapdrop@gmail.com>
2025-08-09 00:30:13 +0200

    Removed video mode detection + regressions
    As it turns out, detecting video mode within 446 B is not a trivial
    task. Furthermore, video modes with more than 80 columns are non-
    standard, and are not guaranteed to exist on all platforms.
    
    With that said, future iterations might attempt to toggle the font
    being used in text mode - while still using 80-column mode.
    
    The sources compile, but have never been tested.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/boot0.asm
+++ boot/src/boot0.asm
@@ -3,9 +3,8 @@
 ; The *absolute* initial stage of every boot sequence
 ; you can find out there in the wild.
 ;
-; Initialize the system, set the video mode, load
-; second stage into 0x8000, enable protected mode
-; and transfer control to second stage.
+; Initialize the system, load second stage into 0x8000,
+; enable protected mode and transfer control to second stage.
 ; 
 ; Boot parameters can be found in 'params' (0x7e00).
 ; - parameters are 16-bit values, which must
@@ -15,14 +14,16 @@
 ; * 0x00500-0x07af0 - stack (to be relocated)
 ; * 0x07b00-0x07bff - guard region (not enforced)
 ; * 0x07c00-0x07dff - MBR
-; * 0x07e00-0x095ff - second-stage loader
-; * 0x09600-0x097ff - guard region (not enforced)
+; * 0x08000-0x097ff - second-stage loader
 ; * 0x09800-0x0ffff - E820 memory map
 ; * 0x10000-0x1ffff - minimum usable memory (=128 kB)
 ; * 0x20000-0x7ffff - maximum usable memory (>128 kB)
 
 [bits 16]
 [org 0x7c00]
+
+ADDR_S2_LDR         equ 0x8000
+ADDR_E820_MAP       equ 0x9800
 
 ; We'll just assume that the very first bytes of the
 ; target MBR are 'EB 3C 90' and work from there.
@@ -109,37 +110,6 @@ _start:
     ; - fall-through - ;
 .next:
 
-; Set video mode - now
-set_vid:
-    ; Try to set video mode to EGA 80x43 (0x17)
-    mov ax, 0x0017                          ; AH=0x00, AL=0x17 - set video mode to 0x17
-    int 0x10                                ; Call BIOS
-
-    ; Confirm video mode
-    mov ax, 0x0f00                          ; AH = 0x0F, AL=* - get current video mode
-    int 0x10                                ; Call BIOS
-    cmp al, 0x17                            ; Check whether the video mode was successfully set
-    je .is_43                               ; If so, skip ahead
-    ; - fall-through on failure - ;
-
-    ; Reset video mode to 80x25 (0x03)
-    mov ax, 0x0003                          ; AH=0x00, AL=0x03 - set video mode to 0x03
-    int 0x10                                ; Call BIOS
-    mov ax, 0x0f00                          ; AH=0x0F, AL=* - get current video mode
-    int 0x10                                ; Call BIOS
-    cmp al, 0x03                            ; Check whether the video mode was successfully set
-    jne panic                               ; If not, panic with whatever mode we have
-    ; - fall-through on success - ;
-
-.is_25:
-    mov ah, 25                              ; 25 rows
-    jmp .end
-.is_43:
-    mov ah, 43                              ; 43 rows
-.end:
-    mov al, 80                              ; 80 columns
-    mov [textmode], ax                      ; Store text mode
-
 ; Scan for memory using E820
 e820_scan:
     xchg bx, bx                             ; Breakpoint
@@ -156,7 +126,7 @@ e820_scan:
     jl panic                                ; Do not proceed if LMA is
                                             ; smaller than 128 kB
 
-    lea edi, [0x9800 + 4]                   ; Store map at 0x9800+4
+    lea edi, [ADDR_E820_MAP + 4]            ; Store map at ADDR_E820_MAP + 4
     xor esi, esi                            ; Zero entry count
 .seek:
     cmp esi, 1024                           ; Do not proceed beyond
@@ -197,13 +167,13 @@ read_s2:
     ; Check lower half (AX * 1 B)
     mov dx, 512                             ; Divide reserved area size by 512 B
     div dx                                  ; (divide value stored in AX by DX)
-    cmp ax, 8                               ; Check quotient
+    cmp ax, 12                              ; Check quotient
     jl panic                                ; Panic if the reserved area is too small
     ; - fall-through - ;
 
 .read_s2_cont:
     ; Read 12 conventional sectors
-    lea bx, [stage_2]                       ; Point BIOS to buffer at 0x7e00
+    lea bx, [ADDR_S2_LDR]                   ; Point BIOS to buffer at ADDR_S2_LDR
     mov ax, 0x020c                          ; Read 12 sectors
     mov cx, 0x0002                          ; Read from LBA 1 (C0, S2)
     mov dh, 0                               ; Read from head 0
@@ -259,10 +229,11 @@ panic:
     lidt [0x7b00]
     int 0x00
 
-align 16
 [bits 32]
+align 32, nop
+
 pm:
-    mov ax, 0x10                            ; Point to kernel data segment
+    mov eax, 0x10                           ; Point to kernel data segment
 
     ; - Set data segments
     mov ds, ax
@@ -287,14 +258,12 @@ pm:
     movzx eax, byte [bootdev]               ; Store boot device number
     push eax
 
-    movzx eax, word [textmode]              ; Store active text mode
-    push eax
-
     lea eax, [OemLabel]                     ; Store BPB location
     push eax
 
     ; Perform near call to loaded program
-    call stage_2
+    lea eax, [ADDR_S2_LDR]
+    call eax
 
     ; Halt (unreachable)
     cli
@@ -302,7 +271,6 @@ pm:
 
 ; Variables
 bootdev     db 0    ; Boot device (used predominantly by the BIOS)
-textmode    dw 0    ; Text mode
 errmsg      db "Boot failed. Replace boot device and press any key to restart.", 0
 
 ; Structures
@@ -328,5 +296,3 @@ zidtr:
 
 times 510-($-$$) db 0                       ; Pad the boot record
 dw 0xaa55                                   ; Boot signature
-
-stage_2:                                    ; stage 2 entry point (0x7e00)
