ce51e6662b91a8fe0db420b61d73ada6ef1c0832
John Isaac Calderon <ogclapdrop@gmail.com>
2025-08-14 19:35:07 +0200

    Added VT RC locking + restored 'VolatileCell'
    The vector table should now be capable of tracking how many
    read-only handles are issued and restricting write access
    based on the lock state.
    
    The type 'VolatileCell' has been restored (or added?) so
    that MMIO can be safely performed without the compiler
    interfering with seemingly trivial memory access patterns.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/structs/wrappers.rs
+++ boot/src/structs/wrappers.rs
@@ -9,6 +9,62 @@ use core::ptr;
 use core::sync::atomic;
 use core::sync::atomic::Ordering;
 
+/* Memory-compatible volatile cell
+
+    This type can be used to guarantee inner mutability
+    and volatile access for in-situ statics (through
+    the write-only handle), though it cannot fully guarantee
+    thread safety or mutability for aliased memory.
+*/
+#[repr(C)]
+pub struct VolatileCell<T>(UnsafeCell<T>);
+
+impl<T: Copy> VolatileCell<T> {
+    // Perform volatile read
+    // This should be safe, as we own the
+    // internal cell - safety hinges on 'T'
+    // implementing 'Copy' or 'Clone'
+    #[inline(always)]
+    pub fn load(&self) -> T {
+        atomic::fence(Ordering::Acquire);
+        let val = unsafe {
+            // - obtain pointer from inner cell, then read
+            ptr::read_volatile(self.0.get())
+        };
+
+        atomic::fence(Ordering::Acquire);
+        val
+    }
+
+    // Perform volatile write
+    // This should be safe, as we own the
+    // internal cell - safety hinges on
+    // the cell being physically writable,
+    // as well as 'T' implementing 'Copy'
+    // or 'Clone'
+    // - immutable 'self' for statics
+    #[inline(always)]
+    pub unsafe fn store(&self, val: T) {
+        atomic::fence(Ordering::Release);
+        unsafe {
+            // - obtain pointer from inner cell, then write
+            ptr::write_volatile(self.0.get(), val);
+        }
+
+        atomic::fence(Ordering::Release);
+    }
+
+    // Get mutable reference to inner cell
+    // - marked as unsafe, as compliance from
+    // the compiler cannot be guaranteed
+    #[inline(always)]
+    pub const unsafe fn get_mut(&self) -> &mut T {
+        // - since we're playing unsafe anyways, why
+        // not perform pointer trickery
+        unsafe { &mut *self.0.get() }
+    }
+}
+
 /*
     Memory-compatible read-only HAL VT entry
 
@@ -22,6 +78,7 @@ pub struct HalVtableEntry<T>(UnsafeCell<T>);
 
 impl<T: Copy> HalVtableEntry<T> {
     // Return new instance of 'HalVtableEntry'
+    #[inline(always)]
     pub const fn new(val: T) -> Self {
         HalVtableEntry(UnsafeCell::new(val))
     }
@@ -30,6 +87,7 @@ impl<T: Copy> HalVtableEntry<T> {
     // This should be safe, as we own the
     // internal cell - safety hinges on 'T'
     // implementing 'Copy' or 'Clone'
+    #[inline(always)]
     pub fn load(&self) -> T {
         atomic::fence(Ordering::Acquire);
         let val = unsafe {
@@ -39,6 +97,15 @@ impl<T: Copy> HalVtableEntry<T> {
 
         atomic::fence(Ordering::Acquire);
         val
+    }
+
+    // Provide access to inner cell
+    // - attempting to modify the inner
+    //   cell without acquiring a write
+    //   lock is UB
+    #[inline(always)]
+    pub const fn get_cell(&self) -> &UnsafeCell<T> {
+        &self.0
     }
 }
 
@@ -60,6 +127,7 @@ impl<T: Copy> HalVtableEntryWriter<T> {
     // This should be safe, as we own the
     // internal cell - safety hinges on 'T'
     // implementing 'Copy' or 'Clone'
+    #[inline(always)]
     pub fn load(&self) -> T {
         atomic::fence(Ordering::Acquire);
         let val = unsafe {
@@ -78,6 +146,7 @@ impl<T: Copy> HalVtableEntryWriter<T> {
     // as well as 'T' implementing 'Copy'
     // or 'Clone'
     // - immutable 'self' for statics
+    #[inline(always)]
     pub unsafe fn store(&self, val: T) {
         atomic::fence(Ordering::Release);
         unsafe {
@@ -91,6 +160,7 @@ impl<T: Copy> HalVtableEntryWriter<T> {
     // Get mutable reference to inner cell
     // - marked as unsafe, as compliance from
     // the compiler cannot be guaranteed
+    #[inline(always)]
     pub const unsafe fn get_mut(&self) -> &mut T {
         // - since we're playing unsafe anyways, why
         // not perform pointer trickery
