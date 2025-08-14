0e1e755fd62bad8c97ae5c09cebc4a1a2045e0b4
John Isaac Calderon <ogclapdrop@gmail.com>
2025-08-14 16:15:19 +0200

    Added HAL-related macros + VT access control
    The macros should make it somewhat easier to define the HAL
    and VT sections, though manual input is still required.
    
    The VT access control struct has been implemented, though
    it hasn't been rigorously tested - it merely compiles.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/lib.rs
+++ boot/src/lib.rs
@@ -1,39 +1,40 @@
 #![no_std]
 #![no_main]
 
-mod structs;
 mod macros;
+mod structs;
 
-use core::mem::drop;
-use core::arch::asm;
-use core::ptr::write_volatile;
 use core::panic::PanicInfo;
-use core::ptr::read_volatile;
-use structs::{BiosPB, HalVectorTable};
+use core::ptr::write_volatile;
+use structs::BiosPB;
 
-// Define HAL
+// Define HAL section
 relocate! {
-    fn inb(port: u16) -> u8 {
+    fn default_inb(port: u16) -> u8 {
         (port as u8) + 3
     } => ".hal";
 
-    fn outb(port: u16, arg: u8) {
+    fn default_outb(port: u16, arg: u8) {
 
     } => ".hal";
 
-    fn inw(port: u16) -> u16 {
+    fn default_inw(port: u16) -> u16 {
         2
     } => ".hal";
 
-    fn outw(port: u16, arg: u16) {
+    fn default_outw(port: u16, arg: u16) {
 
     } => ".hal";
+}
 
-    pub static HAL_VT: HalVectorTable = HalVectorTable {
-        inb,
-        outb,
-        inw,
-        outw,
+// Define HAL vector table section
+// Call template struct 'HalDispatches'
+hal_vt_instance! {
+    pub static HAL_VT: HalDispatches = {
+        inb: extern "C" fn(u16) -> u8 = default_inb,
+        outb: extern "C" fn(u16, u8) = default_outb,
+        inw: extern "C" fn(u16) -> u16 = default_inw,
+        outw: extern "C" fn(u16, u16) = default_outw,
     }; => ".vt_hal";
 }
 
@@ -42,21 +43,15 @@ relocate! {
 #[inline(never)]
 #[unsafe(no_mangle)]
 pub extern "C" fn main(bpb: &BiosPB, bootdev: u64, e820_ptr: *const u8) -> ! {
-    // Just for the sake of laughs and giggles!
-    let vt: *const HalVectorTable = &HAL_VT as *const HalVectorTable as usize as *const HalVectorTable;
-    unsafe { asm!("xchg bx, bx", "nop", options(nomem)); }
-
-    // This dereference might get inlined, so
-    // volatile access is very much preferred.
-    let mut f = unsafe { read_volatile(vt) };
+    let x = HAL_VT.dispatch(|x| &x.inb);
 
     let mut a: u8 = 0;
 
     loop {
-        unsafe { write_volatile(&mut a as *mut u8, a + 2); }
+        unsafe {
+            write_volatile(&mut a as *mut u8, a + 2);
+        }
     }
-
-    drop(f);
 }
 
 #[panic_handler]
