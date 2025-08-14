a67f4f0801bfcff44e329f445deb906acc80458a
John Isaac Calderon <ogclapdrop@gmail.com>
2025-08-14 16:15:19 +0200

    Added HAL-related macros + VT access control
    The macros should make it somewhat easier to define the HAL
    and VT sections, though manual input is still required.
    
    The VT access control struct has been implemented, though
    it hasn't been rigorously tested - it merely compiles.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/macros.rs
+++ boot/src/macros.rs
@@ -3,6 +3,8 @@
     Expect therefore unseen inconsistencies.
 */
 
+// Thise one uses a custom syntax, so there
+// should be no surprises here...
 #[macro_export]
 macro_rules! relocate {
     // Function with args and return type
@@ -91,4 +93,55 @@ macro_rules! relocate {
 
     // Termination case
     () => {};
+}
+
+// Instantiate access-controlled HAL VT
+//
+// The rules here are *slightly* different to idiomatic Rust
+// - $struct_name does NOT actually set the type for $instance,
+//   as it will always be 'HalVtableAC'. We only require
+//   $struct_name so that we can use an unused name
+#[macro_export]
+macro_rules! hal_vt_instance {
+    // Enforce public visibility
+    (
+        $(#[$meta:meta])*
+        pub static $instance:ident : $struct_name:ident = {
+            $(
+                $field:ident : $ty:ty = $val:expr
+            ),* $(,)?
+        }; => $section:literal;
+    ) => {
+        // Define the struct type
+        #[repr(C)]
+        pub struct $struct_name {
+            $(
+                pub $field: $crate::structs::wrappers::HalVtableEntry<$ty>,
+            )*
+        }
+
+        impl $struct_name {
+            pub const fn new(
+                $($field: $ty),*
+            ) -> Self {
+                $struct_name {
+                    $(
+                        $field: $crate::structs::wrappers::HalVtableEntry::new($field),
+                    )*
+                }
+            }
+        }
+
+        // Implement marker trait
+        impl $crate::structs::HalVectorTable for $struct_name {}
+
+        // Define the instance
+        $(#[$meta])*
+        #[unsafe(no_mangle)]
+        #[unsafe(link_section = $section)]
+        pub static $instance: $crate::structs::HalVtableAC<$struct_name> = $crate::structs::HalVtableAC::new(
+            core::sync::atomic::AtomicIsize::new(0),
+            $struct_name::new($($val),*),
+        );
+    };
 }
--- boot/src/structs/wrappers.rs
+++ boot/src/structs/wrappers.rs
@@ -3,3 +3,97 @@
     fully or partially binary-compatible with
     the wrapped types
 */
+
+use core::cell::UnsafeCell;
+use core::ptr;
+use core::sync::atomic;
+use core::sync::atomic::Ordering;
+
+/*
+    Memory-compatible read-only HAL VT entry
+
+    This type can be used to guarantee inner mutability
+    and volatile access for in-situ statics (through
+    the write-only handle), though it cannot fully guarantee
+    thread safety or mutability for aliased memory.
+*/
+#[repr(C)]
+pub struct HalVtableEntry<T>(UnsafeCell<T>);
+
+impl<T: Copy> HalVtableEntry<T> {
+    // Return new instance of 'HalVtableEntry'
+    pub const fn new(val: T) -> Self {
+        HalVtableEntry(UnsafeCell::new(val))
+    }
+
+    // Perform volatile read
+    // This should be safe, as we own the
+    // internal cell - safety hinges on 'T'
+    // implementing 'Copy' or 'Clone'
+    pub fn load(&self) -> T {
+        atomic::fence(Ordering::Acquire);
+        let val = unsafe {
+            // - obtain pointer from inner cell, then read
+            ptr::read_volatile(self.0.get())
+        };
+
+        atomic::fence(Ordering::Acquire);
+        val
+    }
+}
+
+/*
+    Memory-compatible read-write HAL VT entry
+
+    This type can be used to guarantee inner mutability
+    and volatile access for in-situ statics (through
+    the write-only handle), though it cannot fully guarantee
+    thread safety or mutability for aliased memory.
+
+    Can only be obtained through `HalVtableAC`.
+*/
+#[repr(C)]
+pub struct HalVtableEntryWriter<T>(UnsafeCell<T>);
+
+impl<T: Copy> HalVtableEntryWriter<T> {
+    // Perform volatile read
+    // This should be safe, as we own the
+    // internal cell - safety hinges on 'T'
+    // implementing 'Copy' or 'Clone'
+    pub fn load(&self) -> T {
+        atomic::fence(Ordering::Acquire);
+        let val = unsafe {
+            // - obtain pointer from inner cell, then read
+            ptr::read_volatile(self.0.get())
+        };
+
+        atomic::fence(Ordering::Acquire);
+        val
+    }
+
+    // Perform volatile write
+    // This should be safe, as we own the
+    // internal cell - safety hinges on
+    // the cell being physically writable,
+    // as well as 'T' implementing 'Copy'
+    // or 'Clone'
+    // - immutable 'self' for statics
+    pub unsafe fn store(&self, val: T) {
+        atomic::fence(Ordering::Release);
+        unsafe {
+            // - obtain pointer from inner cell, then write
+            ptr::write_volatile(self.0.get(), val);
+        }
+
+        atomic::fence(Ordering::Release);
+    }
+
+    // Get mutable reference to inner cell
+    // - marked as unsafe, as compliance from
+    // the compiler cannot be guaranteed
+    pub const unsafe fn get_mut(&self) -> &mut T {
+        // - since we're playing unsafe anyways, why
+        // not perform pointer trickery
+        unsafe { &mut *self.0.get() }
+    }
+}
