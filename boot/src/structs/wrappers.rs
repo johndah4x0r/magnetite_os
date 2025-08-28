119f033e7779f6990d6bfff69c711b386b6f3767
John Isaac Calderon <ogclapdrop@gmail.com>
2025-08-28 05:15:24 +0200

    Made HAL VT relocatable + removed redundancies
    The vector table struct should now be able to translate function
    pointers internally, so that they point to methods with respect
    to the VT's new base - instead of the old base, or +0x0. This
    approach demands discipline: the VT should ideally be in the same
    section as the methods it is pointing to.
    
    The `dispatch` method no longer returns lock handles. Related
    structs have therefore been removed. Atomic access is instead
    handled internally. This, of course, demands discipline: all
    clients wishing to access the VT must use the same API and
    ABI - but it can be done...
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/structs/wrappers.rs
+++ boot/src/structs/wrappers.rs
@@ -99,71 +99,12 @@ impl<T: Copy> HalVtableEntry<T> {
         val
     }
 
-    // Provide access to inner cell
+    // Provide access to inner data
     // - attempting to modify the inner
     //   cell without acquiring a write
     //   lock is UB
     #[inline(always)]
-    pub const fn get_cell(&self) -> *const UnsafeCell<T> {
-        &self.0
-    }
-}
-
-/*
-    Memory-compatible read-write HAL VT entry
-
-    This type can be used to guarantee inner mutability
-    and volatile access for in-situ statics (through
-    the write-only handle), though it cannot fully guarantee
-    thread safety or mutability for aliased memory.
-
-    Can only be obtained through `HalVtableAC`.
-*/
-#[repr(C)]
-pub struct HalVtableEntryWriter<T>(UnsafeCell<T>);
-
-impl<T: Copy> HalVtableEntryWriter<T> {
-    // Perform volatile read
-    // This should be safe, as we own the
-    // internal cell - safety hinges on 'T'
-    // implementing 'Copy' or 'Clone'
-    #[inline(always)]
-    pub fn load(&self) -> T {
-        atomic::fence(Ordering::Acquire);
-        let val = unsafe {
-            // - obtain pointer from inner cell, then read
-            ptr::read_volatile(self.0.get())
-        };
-
-        atomic::fence(Ordering::Acquire);
-        val
-    }
-
-    // Perform volatile write
-    // This should be safe, as we own the
-    // internal cell - safety hinges on
-    // the cell being physically writable,
-    // as well as 'T' implementing 'Copy'
-    // or 'Clone'
-    // - immutable 'self' for statics
-    #[inline(always)]
-    pub unsafe fn store(&self, val: T) {
-        atomic::fence(Ordering::Release);
-        unsafe {
-            // - obtain pointer from inner cell, then write
-            ptr::write_volatile(self.0.get(), val);
-        }
-
-        atomic::fence(Ordering::Release);
-    }
-
-    // Get mutable reference to inner cell
-    // - marked as unsafe, as compliance from
-    // the compiler cannot be guaranteed
-    #[inline(always)]
-    pub const unsafe fn get_mut(&self) -> &mut T {
-        // - since we're playing unsafe anyways, why
-        // not perform pointer trickery
-        unsafe { &mut *self.0.get() }
+    pub const unsafe fn get(&self) -> *mut T {
+        self.0.get()
     }
 }
