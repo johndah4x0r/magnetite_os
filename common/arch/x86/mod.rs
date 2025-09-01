202f09eedc6f603d5724500bed865dd5bbdb3cb1
John Isaac Calderon <ogclapdrop@gmail.com>
2025-09-01 02:49:58 +0200

    Established shared resources module
    As per decision 2025-08-30, part 2, platform-specific
    definitions must be separated from shared defintions,
    and must be guarded off whenever possible.
    
    For the sake of completeness, the example code in
    `kern/lib.rs` has been replaced with a proper
    template.
    
    A proposed layout for the module, which is being implemented
    by this commit, has been immortalized in decision 2025-09-01,
    part 1.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

---
+++ common/arch/x86/mod.rs
@@ -0,0 +1,8 @@
+/*
+    Definitions specific to the IA-32 and x86-64
+    platforms (known collectively as x86)
+*/
+
+// x86-specific structures, such as the
+// BPB and E820 memory layout entries
+pub mod structs;
