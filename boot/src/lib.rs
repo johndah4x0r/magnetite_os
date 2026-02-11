291a2d93e8395394f45047e0dc36dfc810e38a8f
John Isaac Calderon <ogclapdrop@gmail.com>
2026-02-11 23:09:33 +0100

    added basic VGA console driver + test cases
    The text-mode driver is currently in its provisional stage,
    as invariants are being worked out. It currently works as
    expected, though it is still ripe for future expansion.
    
    Test cases have been added in the stage-2 loader
    executable to demonstrate that text mode works.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- boot/src/lib.rs
+++ boot/src/lib.rs
@@ -8,8 +8,9 @@ extern crate common;
 use common::shared::structs::ArrayLike;
 use core::panic::PanicInfo;
 
-// - x86-specific structures
-use common::arch::x86::structs::{BiosPB, LongE820};
+// - BIOS-specific structures
+use common::plat::pc_bios::structs::{BiosPB, LongE820};
+use common::plat::pc_bios::vga::console::VgaConsole;
 
 // Initial routine
 //  - call it 'main' for the sake of brevity
@@ -17,10 +18,21 @@ use common::arch::x86::structs::{BiosPB, LongE820};
 #[inline(never)]
 #[unsafe(no_mangle)]
 pub extern "C" fn main(
-    _bios_pb: &BiosPB,
+    _bios_pb: &'static BiosPB,
     _bootdev: u64,
     _e820_map: &'static ArrayLike<'static, LongE820>,
 ) -> ! {
+    // Instantiate VGA console
+    let mut console = VgaConsole::defaults();
+
+    // Clear screen
+    console.clear();
+
+    // Write to screen
+    console.write_str("Hello world!\n");
+    console.write_str("This is a test!\n");
+    console.write_str("The quick brown fox jumps over the lazy dog");
+
     loop {}
 }
 
---
+++ common/plat/pc_bios/vga/mod.rs
@@ -0,0 +1,6 @@
+/*
+    Definitions specific to VGA operation on the PC platform
+*/
+
+// VGA text console
+pub mod console;
